b8d349066ef171b4e4e8be9628b3e8b2e8724088
Raman Hafiyatulin <r.gafiyatullin@me.com>
2024-12-08 15:16:06 +0200

    ErrorOf: make use of the blanket-trait ErrorKind

--- crates/mm1-common/src/errors/error_of.rs
+++ crates/mm1-common/src/errors/error_of.rs
@@ -1,22 +1,22 @@
-use std::fmt;
 use std::sync::Arc;
 
+use super::error_kind::ErrorKind;
 use crate::errors::error_kind::HasErrorKind;
 
 #[derive(Debug, thiserror::Error)]
 #[error("{}: {}", kind, message)]
-pub struct ErrorOf<Kind: fmt::Display + Eq + Ord + Copy + Send + Sync + 'static> {
+pub struct ErrorOf<Kind: ErrorKind> {
     pub kind:    Kind,
     pub message: Arc<str>,
 }
 
 impl<K0> ErrorOf<K0>
 where
-    K0: fmt::Display + Eq + Ord + Copy + Send + Sync + 'static,
+    K0: ErrorKind,
 {
     pub fn map_kind<K1>(self, map: impl FnOnce(K0) -> K1) -> ErrorOf<K1>
     where
-        K1: fmt::Display + Eq + Ord + Copy + Send + Sync + 'static,
+        K1: ErrorKind,
     {
         let ErrorOf { kind, message } = self;
         let kind = map(kind);
@@ -24,16 +24,14 @@ where
     }
 }
 
-impl<K: fmt::Display + Eq + Ord + Copy + Send + Sync + 'static> ErrorOf<K> {
+impl<K: ErrorKind> ErrorOf<K> {
     pub fn new(kind: K, message: impl Into<Arc<str>>) -> Self {
         let message = message.into();
         Self { kind, message }
     }
 }
 
-impl<Kind: fmt::Display + Eq + Ord + Copy + Send + Sync + 'static> HasErrorKind<Kind>
-    for ErrorOf<Kind>
-{
+impl<Kind: ErrorKind> HasErrorKind<Kind> for ErrorOf<Kind> {
     fn kind(&self) -> Kind {
         self.kind
     }
